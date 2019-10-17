8b0cdf7e6d3b0f7614023ec0206194c9ddf7b7f4
Odeta <obudryte@gmail.com>
2019-10-17 21:24:20 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './assets/style.css';
+import './assets/style.scss';
+
+
+import Icon from './assets/img/facebook-letter-logo.svg'
+const img = new Image()
+img.src = Icon
+// // element.appendChild(img)
+// document.querySelector('body').appendChild(img);
+
+const div = document.createElement('div');
+document.querySelector('body').appendChild(div);
+document.querySelector('div').appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+//kai plugina isimetu reikia ir sito varo
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+  contentBase: './dist',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
@@ -9,12 +18,34 @@ module.exports = {
   module:{
     rules:[
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-          'style-loader',
+          {
+             loader: MiniCssExtractPlugin.loader,
+             options: {
+               publicPath: (resourcePath, context) => {
+                 return path.relative(path.dirname(resourcePath), context) + '/';
+               },
+             },
+           },
           'css-loader',
+          'sass-loader',
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    //sumeta js i html musu?
+    new HTMLWebpackPlugin(),
+    //istrina kas yra nenaudojama tik dist direktorijoj. logiskaii butu paskutini ji laikyti
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
 }
